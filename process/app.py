b37e86b74f688db3ad52958b6c85083578eae168
zing-y <65362209+zing-y@users.noreply.github.com>
2022-01-07 23:49:15 +0800

    Add files via upload

---
+++ process/app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from flask_cors import CORS
+from flask import Flask, jsonify
+from processdata import processdata
+app = Flask(__name__)
+CORS(app)
+@app.route('/displaylocations')
+def displaylocations():
+    l = processdata()
+    return jsonify(l)
+if __name__ == '__main__':
+    app.run(host = 'localhost', debug = True, port = 8004)
